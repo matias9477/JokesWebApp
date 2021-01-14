6f0420f130706ca127df1428f88f1172b70e02a4
matias <matias.turra@gmail.com>
2021-01-14 10:27:39 -0300

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        //properties, con prop + tab escribe todo y llenas las cosas
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }  
+
+        //constructor  shortcut ctor
+        public Joke()
+        {
+            
+        }
+
+        
+    }
+}
